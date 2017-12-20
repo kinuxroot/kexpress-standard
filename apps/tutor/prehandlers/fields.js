ddb3bae767916065d31cd351705cff1c4c277cb4
kinuxroot <kinuxroot@163.com>
2017-12-20 22:30:31 +0800

    Modify the error handler of fields error

--- apps/tutor/prehandlers/fields.js
+++ apps/tutor/prehandlers/fields.js
@@ -1,13 +1,11 @@
 const ktoolkit = require('ktoolkit');
 const logger = ktoolkit.logger.output;
+const errors = require('../config/errors');
 
 async function errorHandler(req, res, error) {
   logger.error(error);
 
-  res.json({
-    successful: false,
-    error: error
-  });
+  throw new errors.FieldsError(error);
 }
 
 module.exports = {
