1d6b258311dbd7b66c1e1e78046e391f7808449a
kinuxroot <kinuxroot@163.com>
2017-12-27 10:12:26 +0800

    Add description in router

--- apps/tutor/routes/user.js
+++ apps/tutor/routes/user.js
@@ -1,8 +1,10 @@
 const kexpress = require('kexpress');
 const Router = kexpress.core.router.Router;
-const router = new Router();
 const actions = require('../actions/user');
 
+const router = new Router({
+  description: '用户操作'
+});
 router.post('/login', actions.actionLogin);
 router.get('/:userId', actions.actionGetUser);
 router.put('/', actions.actionCreateUser);
