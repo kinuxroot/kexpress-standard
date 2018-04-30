25e0bd8af7725c85c3147c024ff129d3059bd067
kinuxroot <kinuxroot@163.com>
2018-04-30 09:26:29 +0800

    1. Release v1.0.0-alpha1 2. Support phase and plugin

--- templates/app.template.js
+++ templates/app.template.js
@@ -2,47 +2,19 @@
 
 module.exports = {
   name: 'app',
-  prompts: [{
-    type: 'input',
-    name: 'name',
-    message: 'Application name:'
-  }],
-  operations: [{
-    type: 'mv',
-    args: {
-      from: '${projectName}/apps/verify',
-      to: '${projectName}/apps/${template.appName}'
+  phases: {
+    create: {
+      prompts: [{
+        type: 'input',
+        name: 'name',
+        message: 'Application name:'
+      }],
+      operations: []
+    },
+    init: {
+      operations: []
     }
-  }, {
-    type: 'replace',
-    args: {
-      pattern: 'verify',
-      with: '${template.appName}',
-      paths: [
-        '${projectName}/config',
-        '${projectName}/app.startup.js',
-        '${projectName}/apps/${template.appName}'
-      ]
-    }
-  }, {
-    type: 'replace',
-    args: {
-      pattern: 'Tutor',
-      with: {
-        text: '${template.appName}',
-        transform: function(appName) {
-          const parts = appName.split('-').map(part => {
-            return part.slice(0, 1).toUpperCase() + part.slice(1);
-          });
-
-          return parts.join('');
-        }
-      },
-      paths: [
-        '${projectName}/config',
-        '${projectName}/app.startup.js',
-        '${projectName}/apps/${template.appName}'
-      ]
-    }
-  }]
+  },
+  plugins: {
+  }
 };
--- templates/project.template.js
+++ templates/project.template.js
@@ -4,43 +4,52 @@ module.exports = {
   name: 'project',
   subTemplates: {
     app: {
-      initPath: 'apps/{{app.name}}',
+      instantRoot: '{{project.root}}/apps/{{app.name}}',
       template: 'app'
     }
   },
-  prompts: [{
-    type: 'input',
-    name: 'version',
-    message: 'Version:',
-    default: '1.0.0'
-  }, {
-    type: 'input',
-    name: 'author',
-    message: 'Author:',
-    default: ''
-  }, {
-    type: 'license-list',
-    name: 'license',
-    message: 'License:',
-    default: '',
-  }],
-  operations: [{
-    type: 'kexpress.shelljs',
-    args: {
-      command: 'cp',
-      args: [
-        '-R',
-        '{{template.templateRoot}}/*',
-        '{{project.root}}'
-      ]
+  phases: {
+    create: {
+      prompts: [{
+        type: 'input',
+        name: 'version',
+        message: 'Version:',
+        default: '1.0.0'
+      }, {
+        type: 'input',
+        name: 'author',
+        message: 'Author:',
+        default: ''
+      }, {
+        type: 'license-list',
+        name: 'license',
+        message: 'License:',
+        default: '',
+      }],
+      operations: [{
+        type: 'kexpress.shelljs',
+        args: {
+          command: 'cp',
+          args: [
+            '-R',
+            '{{template.templateRoot}}/content/*',
+            '{{project.root}}'
+          ]
+        }
+      }, {
+        type: 'kexpress.handlebars',
+        args: {
+          files: [
+            '{{project.root}}/package.json',
+            '{{project.root}}/app.startup.js'
+          ]
+        }
+      }]
+    },
+    init: {
+      operations: []
     }
-  }, {
-    type: 'kexpress.handlebars',
-    args: {
-      files: [
-        '{{project.root}}/package.json',
-        '{{project.root}}/app.startup.js'
-      ]
-    }
-  }]
+  },
+  plugins: {
+  }
 };
