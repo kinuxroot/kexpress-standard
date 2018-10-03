3b3dd6132c6bc6890b6a4c8b18838ff6286c39ec
kinuxroot <kinuxroot@163.com>
2018-10-03 12:15:01 +0800

    Update project init app name

--- templates/project/content/init/rest/index.js
+++ templates/project/content/init/rest/index.js
@@ -4,8 +4,8 @@ module.exports = {
   name: 'hello',
   version: '1.0.0-alpha.1',
   description: 'Description',
-  basePath: '/hello/v1',
+  basePath: '/{{project.name}}/v1',
   apps: {
-    user: require('./user')
+    account: require('./account')
   }
 };
